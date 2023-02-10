ce14c5eb7851a383d7c62e3190e7b2e6d78a5d7b
Nour_zahraa <32964390+nourzahraa98@users.noreply.github.com>
2023-02-10 18:41:11 +0200

    first-commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config)=> {
+    if(!config.experiments){
+      config.experiments= {};
+    }
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
---
+++ pages/api/graphql.ts
@@ -0,0 +1,27 @@
+import { ApolloServer } from "apollo-server-micro";
+import "reflect-metadata";
+import {buildSchema, Resolver, Query, ObjectType, Field, ID  } from "type-graphql";
+
+
+import { DogsResolver } from "@/src/schema/dogs.resolver";
+
+
+const schema = await buildSchema({
+    resolvers:[DogsResolver],
+});
+
+const server = new ApolloServer({schema,});
+
+export const config = {
+    api: {
+        bodyParser:false,
+    },
+};
+
+const startServer = server.start();
+
+export default async function handler(req,res) {
+    await startServer;
+    await server.createHandler({ path: "/api/graphql"})(req,res);
+    
+}
